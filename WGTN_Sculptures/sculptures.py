e1f672f99ffa04c9c3f312049857561d69811fac
cpbarrett9 <far1999@gmail.com>
2026-01-05 11:48:13 +1300

    initial commit

---
+++ WGTN_Sculptures/sculptures.py
@@ -0,0 +1,15 @@
+import folium
+import pandas as pd
+
+dataframe = pd.read_json("WGTN_Sculptures/data/sculpture.json", encoding="latin-1")
+m = folium.Map(location=[-41.29, 174.78], zoom_start=14)
+
+for _, row in dataframe.iterrows():
+    folium.Marker(
+        location=[row["y"], row["x"]],
+        popup=f"{row['sculpture']} – {row['artist']}",
+    ).add_to(m)
+
+m.save("wellington_sculptures.html")
+
+#this is a comment
